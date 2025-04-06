95275298c306fe5f9ac6dd4e35de28258286e812
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-04-06 19:09:15 -0300

    colocando algumas variaveis

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const venom = require('venom-bot');
+const multer = require('multer');
+const xlsx = require('xlsx');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
 const port = 3000
+
+//upload da planilha 
+const upload = multer({ dest: 'uploads/' });
 
 venom
     .create({
@@ -16,9 +23,16 @@ venom
 
 function start(client){
     app.post("/send-message", async(req, res) => {
-        const {to, message} = req.body
-        await client.sendText(to + '@c.us', message);
-        res.json("mensagem enviada")
+        const {to, amount, name} = req.body
+        
+        const message = `Olá ${name}, você recebeu R$ ${amount} de cashback! Obrigado por comprar com a gente! 🎉`; 
+
+        try {
+            await client.sendText(to + '@c.us', message);
+            res.json("mensagem enviada");
+        } catch (error) {
+            res.status(500).json({ error: 'Erro ao enviar a mensagem', detalhes: error.message });
+        }
     })
 }
 
